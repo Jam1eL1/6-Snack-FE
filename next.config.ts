9d5d8ecaa01c3eec747befb3ec8793f1ddef26cc
Jam1eL1 <23jisu@gmail.com>
2026-08-03 16:19:04 -0700

    chore: update next config for s3 images

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { hostname: "example.com" },
-      { hostname: "https://snack-s3-production.s3.us-west-2.amazonaws.com/products/" },
+      {
+        protocol: "https",
+        hostname: "snack-s3-production.s3.us-west-2.amazonaws.com",
+        pathname: "/products/**",
+      },
     ],
   },
 };
